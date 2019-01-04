3bb4d9ec6bfbd8982705f3d27845385f533b8c40
Arash Gharibi <arash_gharibi@gmx.de>
2019-01-04 11:05:08 +0100

    - Updated the minor build number.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
 const { mix } = require('laravel-mix')
 
-mix.js('src/index.js', 'dist/print.min_v1.0.47.1.js').styles('src/css/print.css', 'dist/print.min_v1.0.47.1.css')
+mix.js('src/index.js', 'dist/print.min_v1.0.47.2.js').styles('src/css/print.css', 'dist/print.min_v1.0.47.2.css')
 
 mix.webpackConfig({
   output: {
